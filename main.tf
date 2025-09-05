7ac6c5ede2c04338e43c4d502cdaf1b9cc5b5a0d
AndyFM806 <leefernandez1245@gmail.com>
2025-09-04 21:00:41 -0500

    chore(terraform): agregar 3 contenedores de Nginx y mapeo de puertos

--- main.tf
+++ main.tf
@@ -24,3 +24,39 @@ resource "docker_network" "mentor_net" {
   name    = "mentor_net"
   driver  = "bridge"
 }
+
+// App containers (app1, app2, app3)
+resource "docker_container" "app1" {
+  name    = "app1"
+  image   = "nginx:stable-alpine3.21-perl"
+  networks_advanced {
+    name  = "docker_network.app_net.name"
+  }
+  ports {
+    internal = 80
+    external = 8001
+  }
+}
+
+resource "docker_container" "app2" {
+  name    = "app2"
+  image   = "nginx:stable-alpine3.21-perl"
+  networks_advanced {
+    name  = "docker_network.app_net.name"
+  }
+  ports {
+    internal = 80
+    external = 8002
+  }
+}
+
+resource "docker_container" "app3" {
+  name    = "app3"
+  image   = "nginx:stable-alpine3.21-perl"
+  networks_advanced {
+    name  = "docker_network.app_net.name"
+  }
+  ports {
+    internal = 80
+    external = 8003
+  }
