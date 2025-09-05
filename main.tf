b4337c5bcb0f2924f6c5b5eda790695442e46741
AndyFM806 <leefernandez1245@gmail.com>
2025-09-04 20:01:42 -0500

    update docker provider version and add nginx container resource

--- main.tf
+++ main.tf
@@ -1,12 +1,21 @@
 terraform {
   required_providers {
     docker = {
-      source  = "kreuzwerker/docker"
-      version = "3.0.2"
+      source = "kreuzwerker/docker"
+      version = "3.6.2"
     }
   }
 }
 
 provider "docker" {
-      
+
+}
+  resource "docker_container" "ubuntu" {
+  name  = "nginx"
+  image = "nginx:stable-alpine3.21-perl"
+  ports {
+    internal = 80
+    external = 3000
+    
+  }
 }
